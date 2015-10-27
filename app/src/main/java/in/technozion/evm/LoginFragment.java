d7b0164df23e7313c4d2a01b39b357ab00eb4423
nagendra <nagendra.ghi@gmail.com>
2015-10-27 10:34:57 +0530

    small changes

--- app/src/main/java/in/technozion/evm/LoginFragment.java
+++ app/src/main/java/in/technozion/evm/LoginFragment.java
@@ -72,4 +72,5 @@ public class LoginFragment extends Fragment {
             //Toast a message
         }
     }
+
 }
--- app/src/main/java/in/technozion/evm/data/URLS.java
+++ app/src/main/java/in/technozion/evm/data/URLS.java
@@ -1,6 +1,6 @@
 package in.technozion.evm.data;
 
 public class URLS {
-    public static final String LOGIN_URL="";
+    public static final String LOGIN_URL="http://technozion.in/accounts/login_mobile";
 
 }
